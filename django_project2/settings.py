ca82653db0eaffdfcdf773d2f90e5417d234228e
vladislava <vlada.kryazh@gmail.com>
2022-11-08 23:56:50 +0200

    Add Static, CSS, Crispy Forms.

--- django_project2/settings.py
+++ django_project2/settings.py
@@ -55,8 +55,13 @@ INSTALLED_APPS = [
     'main.apps.MainConfig',
     'contacts.apps.ContactsConfig',
     'users.apps.UsersConfig',
-    'get_sessions_info.apps.GetSessionsInfoConfig'
+    'get_sessions_info.apps.GetSessionsInfoConfig',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -142,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
